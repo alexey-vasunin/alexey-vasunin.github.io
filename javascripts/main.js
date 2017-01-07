064c824767aa1eb64106d22e270546603b349524
alexey-vasunin <ycukeng90@mail.ru>
2017-01-07 06:06:39 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
-var Output, Promo;
-Promo=prompt("Введите Ваше имя:","");
-alert("Здравствуйте, "+Promo);
+var Login, Password;
+Login=prompt("Введите Ваш логин: ", "admin");
+if (Login=="admin") {
+  Password=prompt("Здравствуйте, admin! Введите пароль: ", "");
+  if(Password=="123") {
+    alert("Вы вошли в систему как "+Login+"!");
+  }
+  else {
+    alert("Неверный пароль.");
+  }
+}
+else alert("Вы не имеете прав администратора.");
